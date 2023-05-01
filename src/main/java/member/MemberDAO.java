730688ce1daf8c3c7a17c9b1968dfcffcd10b272
이효성 <77770303+hyoseoung@users.noreply.github.com>
2023-05-01 16:16:01 +0900

    Add files via upload

--- src/main/java/member/MemberDAO.java
+++ src/main/java/member/MemberDAO.java
@@ -13,7 +13,7 @@ public class MemberDAO extends ConnectDB {
 	
 	public int insertMember(MemberDTO member) {
 		String sql = "insert into MEMBER"
-				+ "	(M_ID,M_NAME,M_BIRTH,M_LEVEL,M_GENDER,P_NUMBER,EMAIL,NICKNAME,PASSWORD,M_TYPECODE,REG_DATE,STATEMENT) "
+				+ "	(M_ID,M_NAME,M_BIRTH,M_LEVEL,M_GENDER,P_NUMBER,EMAIL,NICKNAME,PASSWORD,M_TYPECODE,REG_DATE,STATE) "
 				+ " values(SEQ_MEMBER_NUM.NEXTVAL,?,?,?,?,?,?,SEQ_MEMBER_NUM.NEXTVAL,?,?,SYSDATE,0)";
 		try {
 			psmt = con.prepareStatement(sql);
@@ -59,35 +59,8 @@ public class MemberDAO extends ConnectDB {
 	      } catch (SQLException e) {
 	         e.printStackTrace();
 	      }
-	    return dto;
-	}
-	public MemberDTO getPNum(String pNum,int type) {
-	      String sql="select * from member where p_number=? and m_typecode=?";
-	      MemberDTO dto=null;
-	      try {
-	         psmt = con.prepareStatement(sql);
-	         psmt.setString(1, pNum);
-	         psmt.setString(2, String.valueOf(type));
-	         rs=psmt.executeQuery();
-	         if(rs.next()) {
-	            dto = new MemberDTO();
-	            dto.setBirth(rs.getString("m_birth"));
-	            dto.setEmail(rs.getString("email"));
-	            dto.setGender(rs.getString("m_gender"));
-	            dto.setLevel(rs.getString("m_level"));
-	            dto.setMemberId(rs.getString("m_id"));
-	            dto.setMemberName(rs.getString("m_name"));
-	            dto.setMemberTypeId(rs.getString("m_typecode"));
-	            dto.setNickName(rs.getString("nickname"));
-	            dto.setPassword(rs.getString("password"));
-	            dto.setPhoneNumber(rs.getString("p_number"));
-	            dto.setRegDate(rs.getDate("reg_date"));
-	         }
-	      } catch (SQLException e) {
-	         e.printStackTrace();
-	      }
-	    return dto;
-	}
+	      return dto;
+	   }
 	public List<MemberDTO> getMemberList(Map<String, Object> param) {
 		List<MemberDTO> list = new ArrayList<>();
 		
@@ -99,15 +72,14 @@ public class MemberDAO extends ConnectDB {
 			//query += "where ? like '%?%' ";
 		}
 		query += "order by m_id desc"
-			+ ")";
-			//+ " where pnum between ? and ?";
+			+ ") where pnum between ? and ?";
 		
 		try {
 			psmt = con.prepareStatement(query);
 //			psmt.setString(1, param.get("searchType").toString());
 //			psmt.setString(2, param.get("searchValue").toString());
-//			psmt.setString(3, param.get("start").toString());
-//			psmt.setString(4, param.get("end").toString());
+			psmt.setString(1, param.get("start").toString());
+			psmt.setString(2, param.get("end").toString());
 			rs = psmt.executeQuery();
 			
 			while(rs.next()) {
@@ -127,29 +99,65 @@ public class MemberDAO extends ConnectDB {
 			}
 		}
 		catch (Exception e) {
-			System.out.println("게시물 목록 조회 중 에러");
+			System.out.println("회원 목록 조회 중 에러");
 			e.printStackTrace();
 		}
 		
 		return list;
 	}
 	
+	public boolean checkPass(String password, String email) {
+	       boolean result = false;
+	       try {
+	           String sql = "SELECT * FROM MEMBER WHERE EMAIL = ? AND PASSWORD = ?";
+	           psmt = con.prepareStatement(sql);
+	           psmt.setString(1, email);
+	           psmt.setString(2, password);
+	           rs = psmt.executeQuery();
+	           if (rs.next()) {
+	               result = true;
+	           }
+	       } catch (Exception e) {
+	           e.printStackTrace();
+	       }
+	       return result;
+	   }
+	   
+	public int updateInfo(MemberDTO dto) {
+	      int result = 0;
+	      try {
+	         String sql = "UPDATE MEMBER SET M_NAME = ?, NICKNAME = ?, PASSWORD = ?, P_NUMBER = ?, M_LEVEL = ? WHERE EMAIL = ?";
+	         psmt=con.prepareStatement(sql);
+	           psmt.setString(1, dto.getMemberName());
+	           psmt.setString(2, dto.getNickName());
+	           psmt.setString(3, dto.getPassword());
+	           psmt.setString(4, dto.getPhoneNumber());
+	           psmt.setString(5, dto.getLevel());
+	           psmt.setString(6, dto.getEmail());
+	           result = psmt.executeUpdate();
+	      } catch (Exception e) {
+	         System.out.println("업데이트 중 오류");
+	         e.printStackTrace();
+	      }
+	      return result;      
+	   }
+	
+
 	public int updateMember(MemberDTO dto) {
 		int res = 0;
 		String query = "update member "
-				+ "set M_NAME=?, PASSWORD=?, P_NUMBER=?, EMAIL=?, NICKNAME=?, M_LEVEL=?, M_GENDER=? "
+				+ "set M_NAME=?, P_NUMBER=?, EMAIL=?, NICKNAME=?, M_LEVEL=?, M_GENDER=? "
 				+ "where M_ID=?";
 		
 		try {
 			psmt = con.prepareStatement(query);
 			psmt.setString(1, dto.getMemberName());
-			psmt.setString(2, dto.getPassword());
-			psmt.setString(3, dto.getPhoneNumber());
-			psmt.setString(4, dto.getEmail());
-			psmt.setString(5, dto.getNickName());
-			psmt.setString(6, dto.getLevel());
-			psmt.setString(7, dto.getGender());
-			psmt.setString(8, dto.getMemberId());
+			psmt.setString(2, dto.getPhoneNumber());
+			psmt.setString(3, dto.getEmail());
+			psmt.setString(4, dto.getNickName());
+			psmt.setString(5, dto.getLevel());
+			psmt.setString(6, dto.getGender());
+			psmt.setString(7, dto.getMemberId());
 			res = psmt.executeUpdate();
 		}
 		catch (Exception e) {
@@ -196,5 +204,136 @@ public class MemberDAO extends ConnectDB {
 		}
 		
 		return totalCount;
+	}
+	
+	public int deleteInfo(String email) {
+	      int result = 0;
+	      try {
+	         String sql="delete from member where email=?";
+	         psmt=con.prepareStatement(sql);
+	         psmt.setString(1, email);
+	         result=psmt.executeUpdate();
+	         System.out.println(result);
+	      } catch (Exception e) {
+	         System.out.println("회원 탈퇴 중 에러");
+	         e.printStackTrace();
+	      }
+	      return result;
+	}
+	
+	public boolean confirmPassword(String password) {
+	         boolean isRight = false;
+	         try {
+	            String sql = "select count(*) from fileboard where password=? and m_id=?";
+	            psmt = con.prepareStatement(sql);
+	            psmt.setString(1, password);
+	            rs = psmt.executeQuery();
+	            rs.next();
+	            if(rs.getInt(1)==1) {
+	               isRight = true;
+	            }
+	         } catch (Exception e) {
+	            isRight = false;
+	            System.out.println("암호 검증 중 에러");
+	            e.printStackTrace();
+	         }
+	         return isRight;
+	      }
+	   
+	public String searchId(MemberDTO dto) {
+        String result = null;
+        String sql = "select email from member where m_name=? and p_number=?";      
+        try {
+           psmt = con.prepareStatement(sql);
+           psmt.setString(1,dto.getMemberName());
+           psmt.setString(2, dto.getPhoneNumber());          
+           rs = psmt.executeQuery();
+           if(rs.next()) {
+        	   result = rs.getString("email");      
+           }         
+        }catch (SQLException e) {
+           e.printStackTrace();
+           System.out.println(e.getMessage());
+        }
+              
+        return result;
+     }
+     public String searchPw(MemberDTO dto) {
+        String result = null;
+        String email= dto.getEmail();
+        
+        String sql = "select password from member where email=? ";
+        try {
+           psmt = con.prepareStatement(sql);
+           psmt.setString(1, email);
+           rs = psmt.executeQuery();
+           if(rs.next()) {            	
+           	result = rs.getString("password");
+           }
+           System.out.println(result);
+        }catch (SQLException e) {
+           e.printStackTrace();
+           System.out.println(e.getMessage());
+        }
+              
+        return result;
+     }
+
+   															
+   public MemberDTO getPNum(String pNum,int type) {
+	      String sql="select * from member where p_number=? and m_typecode=?";
+	      MemberDTO dto=null;
+	      try {
+	         psmt = con.prepareStatement(sql);
+	         psmt.setString(1, pNum);
+	         psmt.setString(2, String.valueOf(type));
+	         rs=psmt.executeQuery();
+	         if(rs.next()) {
+	            dto = new MemberDTO();
+	            dto.setBirth(rs.getString("m_birth"));
+	            dto.setEmail(rs.getString("email"));
+	            dto.setGender(rs.getString("m_gender"));
+	            dto.setLevel(rs.getString("m_level"));
+	            dto.setMemberId(rs.getString("m_id"));
+	            dto.setMemberName(rs.getString("m_name"));
+	            dto.setMemberTypeId(rs.getString("m_typecode"));
+	            dto.setNickName(rs.getString("nickname"));
+	            dto.setPassword(rs.getString("password"));
+	            dto.setPhoneNumber(rs.getString("p_number"));
+	            dto.setRegDate(rs.getDate("reg_date"));
+	         }
+	      } catch (SQLException e) {
+	         e.printStackTrace();
+	      }
+	    return dto;
+	}
+   public int updatePw(MemberDTO dto) {
+ 	  int result = 0;
+ 	  String sql = "update member set password =? where email=?";
+ 	  try {
+ 		  psmt = con.prepareStatement(sql);
+ 		  psmt.setString(1, dto.getPassword());
+ 		  psmt.setString(2, dto.getEmail());
+ 		  
+ 		  result = psmt.executeUpdate();
+ 	  }catch(Exception e) {
+ 		  e.printStackTrace();
+ 	  }
+ 	  return result;
+   }
+	public int updateState(String id,String type,String state) {
+		String sql ="update member set state = ? where m_id=? and m_typecode=?";
+		try {
+			psmt=con.prepareStatement(sql);
+			psmt.setString(1, state);
+			psmt.setString(2, id);
+			psmt.setString(3, type);
+			return psmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+
 	}
 }
